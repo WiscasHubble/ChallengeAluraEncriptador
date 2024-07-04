2f39571501ca9d20c31208ce8cb8586586698bb5
WiscasHubble <luis.acevedosl@uanl.edu.mx>
2024-07-04 17:55:51 -0600

    funcion copiar contenido

--- Assets/Scripts/scripts.js
+++ Assets/Scripts/scripts.js
@@ -31,6 +31,7 @@ function encriptarTexto() {
             .replace(/u/g, 'ufat');
 
         console.log(encryptedText);
+        mostrarResultados(encryptedText);
 
         return encryptedText;
 
@@ -38,7 +39,6 @@ function encriptarTexto() {
         console.error("No se ha seleccionado ningún texto");
     }
 }
-
 
 function desencriptarTexto() {
     const textToDecrypt = selectText();
@@ -60,6 +60,37 @@ function desencriptarTexto() {
     }
 }
 
+function mostrarResultados(encryptedText) {
+    // Seleccionamos el aside por su ID y vaciamos su contenido
+    const resultadosAside = document.getElementById('resultados');
+    resultadosAside.innerHTML = '';
+
+    // Creamos un nuevo div con una clase llamada 'contenidoResultado' usando template strings
+    const newContent = `
+        <div class="contenidoResultado">
+            <div class="contenidoResultadoParrafo" id = "contenidoResultadoParrafo"><p>${encryptedText}</p></div>
+            <div class="copiarResultado"><button class="copiar" id = "copiarBtn">Copiar</button></div>
+        </div>
+    `;
+
+    // Insertamos el nuevo contenido en el aside
+    resultadosAside.innerHTML = newContent;
+
+    document.getElementById('copiarBtn').onclick = copiarTexto; //asignamos el evento una vez se crea el boton
+}
+
+function copiarTexto() {
+    var content = document.getElementById('contenidoResultadoParrafo').textContent;
+    navigator.clipboard.writeText(content).then(() => {
+        console.log("Texto copiado al portapapeles!");
+    }).catch(err => {
+        console.error("Error al copiar el texto: ", err);
+    });
+}
+
+
+
 //llamamos aquí a los eventos para no tener que escribirlos en el html y tener un mejor control sobre ellos
 document.getElementById('encriptarBtn').onclick = encriptarTexto;
 document.getElementById('desencriptarBtn').onclick = desencriptarTexto;
+
